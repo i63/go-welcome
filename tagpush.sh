727e75f97515c2b2e75800d1f70f54f536df0ba9
Chakradhar Rao Jonagam <9chakri+ds@gmail.com>
2017-06-17 13:57:15 -0700

    update

--- tagpush.sh
+++
@@ -1,3 +0,0 @@
-docker build -t "gotest" .
-docker tag gotest docker-registry-default.cloud.expertsfactory.com/lab/gotest:latest
-docker push docker-registry-default.cloud.expertsfactory.com/lab/gotest:latest
