0226e689f1e9976331c4ff46455c814d98c0a5e4
tetradev1 <9chakri+ds@gmail.com>
2017-06-24 15:46:23 -0700

    updat

--- script.sql
+++ script.sql
@@ -1,4 +1,4 @@
-#create database store;
+create database store;
 use store;
 create table inventory (id int,product_id varchar(30),product_cost int,product_availabilty int,product_subcat int);
 insert into inventory values (1,'cable_1',10,200,1);
